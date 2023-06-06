45c959627279426d7a55346a2888734965dc29c2
Harshavardhanpentakota <129784884+Harshavardhanpentakota@users.noreply.github.com>
2023-06-06 21:34:29 +0530

    Add files via upload
    12.Count_Inversions.

---
+++ 12.Count_Inversions.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+long long getInversions(long long *arr, int n){
+    int count=0;
+    for(int i=0;i<n;i++)
+        for(int j=0;j<i;j++)
+          if(arr[i]<arr[j])
+            count++;
+    return count;
+}
